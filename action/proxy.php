a743f21f1580c39bd604399b8b757bb2a0a8cedf
worldace <tokaiknight@yahoo.co.jp>
2016-05-10 00:57:24 +0900

    no message

--- action/proxy.php
+++ action/proxy.php
@@ -1,11 +1,14 @@
 <?php
+//allow_url_fopenがonである必要がある。参考:http://doremi.s206.xrea.com/php/tips/http.html
+
 //PHPの実行時間制限(単位は秒、0で無制限)
 ini_set("max_execution_time", 3600);
 
-//allow_url_fopenがonである必要がある。参考:http://doremi.s206.xrea.com/php/tips/http.html
 
 if(!isset($_GET['url'])){ error(); }
 if(!preg_match("|^https*://|i", $_GET['url'])){ error(); }
+if(is_sameorigin($_GET['url'])){ error(); }
+
 
 foreach(getallheaders() as $name => $value){
     if(preg_match("/^Host$/i", $name)){ continue; }
@@ -45,4 +48,21 @@ fclose($fp);
 function error(){
     header('HTTP', true, 400);
     exit;
+}
+
+
+function is_sameorigin($url){
+    $query = parse_url($url); //$query['scheme'] $query['host'] $query['port']
+ 
+    if(!isset($query['port'])){
+        $query['port'] = ($query['scheme'] == "http") ? 80 : 443;
+    }
+
+    $server = array();
+    $server['host'] = $_SERVER['HTTP_HOST'];
+    $server['port'] = $_SERVER['SERVER_PORT'];
+    $server['scheme'] = isset($_SERVER["HTTPS"]) ? "https" : "http";
+    
+    if(($server['scheme'] == $query['scheme']) and ($server['host'] == $query['host']) and ($server['port'] == $query['port'])){ return true; }
+    else{ return false; }
 }
