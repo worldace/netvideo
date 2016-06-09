8262abba5794cbbeb973877e73bd67e3d9cb706b
worldace <tokaiknight@yahoo.co.jp>
2016-06-09 13:59:20 +0900

    no message

--- action/parts/player.php
+++ action/parts/player.php
@@ -65,8 +65,8 @@ $v.controller.parts = {
     commentoff: "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTc5MiIgaGVpZ2h0PSIxNzkyIiB2aWV3Qm94PSIwIDAgMTc5MiAxNzkyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Im0xNzkyLDc5MnEwLDE3NCAtMTIwLDMyMS41dC0zMjYsMjMzdC00NTAsODUuNXEtNzAsMCAtMTQ1LC04cS0xOTgsMTc1IC00NjAsMjQycS00OSwxNCAtMTE0LDIycS0xNywyIC0zMC41LC05dC0xNy41LC0yOWwwLC0xcS0zLC00IC0wLjUsLTEydDIsLTEwdDQuNSwtOS41bDYsLTlsNywtOC41bDgsLTlxNywtOCAzMSwtMzQuNXQzNC41LC0zOHQzMSwtMzkuNXQzMi41LC01MXQyNywtNTl0MjYsLTc2cS0xNTcsLTg5IC0yNDcuNSwtMjIwdC05MC41LC0yODFxMCwtMTMwIDcxLC0yNDguNXQxOTEsLTIwNC41MDA3OTN0Mjg2LC0xMzYuNDk5Nzg2dDM0OCwtNTAuNDk5ODE3cTI0NCwwIDQ1MCw4NS40OTk2OHQzMjYsMjMzLjAwMDM4MXQxMjAsMzIxLjUwMDMzNnoiIGZpbGw9IiNmZmYiLz48L3N2Zz4=",
     fullscreen: "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTc5MiIgaGVpZ2h0PSIxNzkyIiB2aWV3Qm94PSIwIDAgMTc5MiAxNzkyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGQ9Ik04ODMgMTA1NnEwIDEzLTEwIDIzbC0zMzIgMzMyIDE0NCAxNDRxMTkgMTkgMTkgNDV0LTE5IDQ1LTQ1IDE5aC00NDhxLTI2IDAtNDUtMTl0LTE5LTQ1di00NDhxMC0yNiAxOS00NXQ0NS0xOSA0NSAxOWwxNDQgMTQ0IDMzMi0zMzJxMTAtMTAgMjMtMTB0MjMgMTBsMTE0IDExNHExMCAxMCAxMCAyM3ptNzgxLTg2NHY0NDhxMCAyNi0xOSA0NXQtNDUgMTktNDUtMTlsLTE0NC0xNDQtMzMyIDMzMnEtMTAgMTAtMjMgMTB0LTIzLTEwbC0xMTQtMTE0cS0xMC0xMC0xMC0yM3QxMC0yM2wzMzItMzMyLTE0NC0xNDRxLTE5LTE5LTE5LTQ1dDE5LTQ1IDQ1LTE5aDQ0OHEyNiAwIDQ1IDE5dDE5IDQ1eiIgZmlsbD0iI2ZmZiIvPjwvc3ZnPg=="
 };
-$v.controller.timeSeeker.isDragging   = false;
-$v.controller.volumeSeeker.isDragging = false;
+$v.controller.timeSeeker.isMoving   = false;
+$v.controller.volumeSeeker.isMoving = false;
 
 
 $v.comment = {};
@@ -316,13 +316,13 @@ $v.controller.setTime = function(time, element){
 };
 
 $v.controller.timeSeeker.mousemoveEvent = function(event, seek){
-    if(!$v.controller.timeSeeker.isDragging){ return; }
+    if(!$v.controller.timeSeeker.isMoving){ return; }
     var percent = $v.controller.setSeeker($v.controller.timeSeekbar, $v.controller.timeSeeker, event.clientX);
     if(seek){ $v.video.currentTime = $v.video.duration * percent; }
 };
 
 $v.controller.volumeSeeker.mousemoveEvent = function(event){
-    if(!$v.controller.volumeSeeker.isDragging){ return; }
+    if(!$v.controller.volumeSeeker.isMoving){ return; }
     $v.video.volume = $v.controller.setSeeker($v.controller.volumeSeekbar, $v.controller.volumeSeeker, event.clientX);
 };
 
@@ -387,7 +387,7 @@ $v.video.addEventListener('timeupdate', function(){
     var sec_now = Math.floor($v.video.currentTime);
     if(sec_now !== $v.video.beforeTime){
         $v.controller.setTime(sec_now, $v.controller.timeCurrent);
-        if(!$v.controller.timeSeeker.isDragging){
+        if(!$v.controller.timeSeeker.isMoving){
             $v.controller.setSeeker($v.controller.timeSeekbar, $v.controller.timeSeeker, sec_now/$v.video.duration);
         }
         //コメント放出
@@ -498,13 +498,13 @@ document.getElementById("controller-time-seek").addEventListener('click', functi
 });
 
 $v.controller.timeSeeker.addEventListener('mousedown', function(event){
-    $v.controller.timeSeeker.isDragging = true;
+    $v.controller.timeSeeker.isMoving = true;
     document.addEventListener('mousemove', $v.controller.timeSeeker.mousemoveEvent);
 });
 
 document.addEventListener('mouseup', function(event){
     $v.controller.timeSeeker.mousemoveEvent(event, true);
-    $v.controller.timeSeeker.isDragging = false;
+    $v.controller.timeSeeker.isMoving = false;
     document.removeEventListener('mousemove', $v.controller.timeSeeker.mousemoveEvent);
 });
 
@@ -517,13 +517,13 @@ document.getElementById("controller-volume-seek").addEventListener('click', func
 
 
 $v.controller.volumeSeeker.addEventListener('mousedown', function(event){
-    $v.controller.volumeSeeker.isDragging = true;
+    $v.controller.volumeSeeker.isMoving = true;
     document.addEventListener('mousemove', $v.controller.volumeSeeker.mousemoveEvent);
 });
 
 document.addEventListener('mouseup', function(event){
     $v.controller.volumeSeeker.mousemoveEvent(event);
-    $v.controller.volumeSeeker.isDragging = false;
+    $v.controller.volumeSeeker.isMoving = false;
     document.removeEventListener('mousemove', $v.controller.volumeSeeker.mousemoveEvent);
 });
 
