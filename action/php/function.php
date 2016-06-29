2657810c70cbac816dd3c38e6b9d9c89149d1b93
worldace <tokaiknight@yahoo.co.jp>
2016-06-29 17:22:31 +0900

    no message

--- action/php/function.php
+++ action/php/function.php
@@ -41,12 +41,18 @@ function 部品初期化(){
             $buf = ob_get_contents();
             ob_end_clean();
             if($設定['追加js']){
-                $js  = implode($設定['追加js'], "\n");
-                $buf = substr_replace($buf, "\n<script>\n$js</script>\n", strrpos($buf,"</body>"), 0); //最後に出現する</body>の前にJSを挿入する
+                $js     = implode($設定['追加js'], "\n");
+                $js_pos = strrpos($buf,"</body>");
+                if($js_pos !== false){
+                    $buf = substr_replace($buf, "\n<script>\n$js</script>\n", $js_pos, 0); //最後に出現する</body>の前にJSを挿入する
+                }
             }
             if($設定['追加css']){
-                $css = implode($設定['追加css'], "\n");
-                $buf = substr_replace($buf, "\n<style>\n$css</style>\n", strpos($buf,"</head>"), 0);   //最初に出現する</head>の前にCSSを挿入する
+                $css     = implode($設定['追加css'], "\n");
+                $css_pos = strpos($buf,"</head>");
+                if($css_pos !== false){
+                    $buf = substr_replace($buf, "\n<style>\n$css</style>\n", $css_pos, 0); //最初に出現する</head>の前にCSSを挿入する
+                }
             }
             print $buf;
         }
