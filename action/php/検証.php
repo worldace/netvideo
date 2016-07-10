995fed42e798499b35438a3176060fa7e532aa1d
worldace <tokaiknight@yahoo.co.jp>
2016-07-11 03:20:14 +0900

    no message

--- action/php/検証.php
+++ action/php/検証.php
@@ -48,34 +48,39 @@ class 検証{
         return (getimagesize("data:;base64,".$this->value)[0] > 0) ? $this->成功() : $this->失敗($comment); //getimagesize()[0]:横サイズ [1]:縦サイズ [2]:GIFは1、JPEGは2、PNGは3
     }
 
-    private function 字以上($num, $comment = ""){
-        if(!$comment and $this->key){ $comment = "{$this->key}は{$num}文字以上にしてください"; }
-        return (mb_strlen($this->value,"UTF-8") < $num) ? $this->成功() : $this->失敗($comment);
+    public function と同じ($value, $comment = ""){ //非文書化
+        if(!$comment and $this->key){ $comment = "{$this->key}が{$value}でありません"; }
+        return ($this->value == $value) ? $this->成功() : $this->失敗($comment);
     }
 
-    private function 字以下($num, $comment = ""){
-        if(!$comment and $this->key){ $comment = "{$this->key}は{$num}文字以内にしてください"; }
-        return (mb_strlen($this->value,"UTF-8") > $num) ? $this->成功() : $this->失敗($comment);
-    }
-
-    private function 以上($num, $unit = "", $comment = ""){
+    public function 以上($num, $unit = "", $comment = ""){ //非文書化
         if(!$comment and $this->key){ $comment = "{$this->key}は{$num}{$unit}以上にしてください"; }
         return (is_numeric($this->value) and ($this->value >= $num)) ? $this->成功() : $this->失敗($comment);
     }
 
-    private function 以下($num, $unit = "", $comment = ""){
+    public function 以下($num, $unit = "", $comment = ""){ //非文書化
         if(!$comment and $this->key){ $comment = "{$this->key}は{$num}{$unit}以下にしてください"; }
         return (is_numeric($this->value) and ($this->value <= $num)) ? $this->成功() : $this->失敗($comment);
     }
 
-    private function より大きい($num, $unit = "", $comment = ""){
+    public function より大きい($num, $unit = "", $comment = ""){ //非文書化
         if(!$comment and $this->key){ $comment = "{$this->key}は{$num}{$unit}より大きくしてください"; }
         return (is_numeric($this->value) and ($this->value > $num)) ? $this->成功() : $this->失敗($comment);
     }
 
-    private function より小さい($num, $unit = "", $comment = ""){
+    public function より小さい($num, $unit = "", $comment = ""){ //非文書化
         if(!$comment and $this->key){ $comment = "{$this->key}は{$num}{$unit}より小さくしてください"; }
         return (is_numeric($this->value) and ($this->value < $num)) ? $this->成功() : $this->失敗($comment);
+    }
+
+    public function 字以上($num, $comment = ""){ //非文書化
+        if(!$comment and $this->key){ $comment = "{$this->key}は{$num}文字以上にしてください"; }
+        return (mb_strlen($this->value,"UTF-8") < $num) ? $this->成功() : $this->失敗($comment);
+    }
+
+    public function 字以下($num, $comment = ""){ //非文書化
+        if(!$comment and $this->key){ $comment = "{$this->key}は{$num}文字以内にしてください"; }
+        return (mb_strlen($this->value,"UTF-8") > $num) ? $this->成功() : $this->失敗($comment);
     }
 
     private function 成功(){
@@ -89,12 +94,15 @@ class 検証{
     }
 
     function __call($name, $args){
-        if     (preg_match("/^([０-９]+)文?字以上$/u", $name, $m)) { $this->字以上(mb_convert_kana($m[1], "n", "utf-8"), $args[0]); }
-        else if(preg_match("/^([０-９]+)文?字以/u", $name, $m))    { $this->字以下(mb_convert_kana($m[1], "n", "utf-8"), $args[0]); }
-        else if(preg_match("/^([０-９]+)(\w*)以上$/u", $name, $m)) { $this->以上(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
-        else if(preg_match("/^([０-９]+)(\w*)以/u", $name, $m))    { $this->以下(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
-        else if(preg_match("/^([０-９]+)(\w*)より大/u", $name, $m)){ $this->より大きい(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
-        else if(preg_match("/^([０-９]+)(\w*)より小/u", $name, $m)){ $this->より小きい(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
-        else { throw new Exception("検証関数に{$name}はありません"); }
+        if     (preg_match("/^([０-９]+)文?字以上$/u", $name, $m)) { return $this->字以上(mb_convert_kana($m[1], "n", "utf-8"), $args[0]); }
+        else if(preg_match("/^([０-９]+)文?字以/u", $name, $m))    { return $this->字以下(mb_convert_kana($m[1], "n", "utf-8"), $args[0]); }
+        else if(preg_match("/^([０-９]+)(\w*)以上$/u", $name, $m)) { return $this->以上(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
+        else if(preg_match("/^([０-９]+)(\w*)以/u", $name, $m))    { return $this->以下(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
+        else if(preg_match("/^([０-９]+)(\w*)より大/u", $name, $m)){ return $this->より大きい(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
+        else if(preg_match("/^([０-９]+)(\w*)より小/u", $name, $m)){ return $this->より小きい(mb_convert_kana($m[1], "n", "utf-8"), $m[2], $args[0]); }
+        else {
+            $name = mb_convert_kana($name, "n", "utf-8");
+            return $this->と同じ($name, $args[0]);
+        }
     }
 }
