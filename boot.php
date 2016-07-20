c1f6bc429f471dad262dac32b161a2846dab671a
worldace <tokaiknight@yahoo.co.jp>
2016-07-20 16:11:22 +0900

    no message

--- boot.php
+++ boot.php
@@ -15,17 +15,18 @@
 //■以下アプリ固有関数
 
 //□第1引数
-//・URLを構成するクエリ。"?action=video&id=10" という先頭が?の文字列か、 ["action"=>"video", "id"=>"10"] という連想配列
-//・URLを構成する文字列。"test/index.html"という先頭が?以外の文字列
-//・引数を省略したときはホームURLが返る
+//"test/index.html"という先頭が?以外の文字列
+//"?action=video&id=10" という先頭が?の文字列
+// ["action"=>"video", "id"=>"10"] という連想配列
+//引数を省略したときはホームURLが返る
 
-//□以下の2点の設定が必要
+//□以下の4点の設定が必要
 //$設定['URL.ホーム'] → ホームページのURL
 //$設定['URL.短縮']   → 短縮URLを有効にするかどうかの真偽値
+//$設定['URL.短縮名'] → 短縮URLの対象となるアクション名
+//$設定['URL.短縮値'] → 短縮URLの対象となる値のキー名
 function URL作成($query = ""){
     global $設定;
-    $短縮対象アクション名 = "video";
-    $短縮対象id名 = "id";
 
     $base = (preg_replace("|/$|", $設定['URL.ホーム'])) ? $設定['URL.ホーム'] : dirname($設定['URL.ホーム'])."/";
 
@@ -33,10 +34,10 @@ function URL作成($query = ""){
         if(preg_match("/^\?/", $query)){ parse_str(substr($query,1), $query); }
         else{ return ($query) ? $base.preg_replace("|^/|","",$query) : $設定['URL.ホーム']; }
     }
-    if($query["action"] === $短縮対象アクション名 and $query[$短縮対象id名] and $設定['URL.短縮']){
-        $短縮対象id値 = rawurlencode($query[$短縮対象id名]);
+    if($query["action"] === $設定['URL.短縮名'] and $query[$設定['URL.短縮値']] and $設定['URL.短縮']){
+        $短縮対象id値 = rawurlencode($query[$設定['URL.短縮値']]);
         unset($query["action"]);
-        unset($query[$短縮対象id名]);
+        unset($query[$設定['URL.短縮値']]);
     }
     if(count($query)){ $output_query = "?" . http_build_query($query, "", "&"); }
     return ($短縮対象id値) ? $base.$短縮対象id値.$output_query : $設定['URL.ホーム'].$output_query;
