d639be7385e8713f806c19909aa3910d00ad8085
worldace <tokaiknight@yahoo.co.jp>
2016-08-11 00:51:22 +0900

    no message

--- action/parts/jsplayer.php
+++ action/parts/jsplayer.php
@@ -767,10 +767,9 @@ $v.ajax = function(url, option){ //method, data, timeout, credential, header, su
     if(option.timeout >= 0){ xhr.timeout = option.timeout * 1000; }
     if(option.credential){ xhr.withCredentials = true; }
     if(option.header){
-        for(var name in option.header){
-            if(!option.header.hasOwnProperty(name)){ continue; }
-            xhr.setRequestHeader(name, option.header[name]);
-        }
+        Object.keys(option.header).forEach(function(key){
+            xhr.setRequestHeader(key, option.header[key]);
+        });
     }
     if(option.method.match(/^POST$/i)){
         if(option.data instanceof FormData){
@@ -778,10 +777,9 @@ $v.ajax = function(url, option){ //method, data, timeout, credential, header, su
         }
         else{
             body = "";
-            for(var key in option.data){
-                if(!option.data.hasOwnProperty(key)){ continue; }
+            Object.keys(option.data).forEach(function(key){
                 body += encodeURIComponent(key) + "=" + encodeURIComponent(option.data[key]) + "&";
-            }
+            });
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
         }
     }
