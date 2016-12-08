b17715904a4cb4860782f507c40bffb3d6e5f4cc
worldace <tokaiknight@yahoo.co.jp>
2016-12-09 00:31:48 +0900

    no message

--- action/lib/function.php
+++ action/lib/function.php
@@ -188,7 +188,16 @@ function 現在のURL($no_query = false){
 }
 
 
-function ベースURL($url){
+function ホームURL($url){
+    $parsed_url = parse_url($url);
+    $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+    $host   = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+    $port   = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+    return $scheme . $host . $port . "/";
+}
+
+
+function 親URL($url){
     return (substr_count($url, "/") === 2) ? $url."/" : dirname($url."a")."/";
 }
 
