a68e8017c750e158b0b3b28d4fb1fc1ef108354b
worldace <tokaiknight@yahoo.co.jp>
2017-02-03 16:33:40 +0900

    no message

--- action/commentpost.php
+++ action/commentpost.php
@@ -7,15 +7,16 @@
 検証("POST", "本文", "64文字以下");
 
 
+$db = "{$_ENV['ディレクトリ.upload']}/". date('Y/md', $_GET['登録時間']) . "/{$_GET['id']}.db";
+if(!is_file($db)){ exit; }
+
+
 データベース("動画") -> 更新($_POST['id'], [
     "コメント数" => ["式"=>"コメント数+1"],
 ]);
 
-$db = "{$_ENV['ディレクトリ.upload']}/". date('Y/md', $_GET['登録時間']) . "/{$_GET['id']}.db";
-if(!is_file($db)){ exit; }
-
 データベース("コメント", "sqlite:$db") -> 追加([
     "本文"     => $_POST['本文'],
     "位置"     => (int)$_POST['位置'],
-    "登録時間" => (int)$_SERVER['REQUEST_TIME'],
+    "登録時間" => $_SERVER['REQUEST_TIME'],
 ]);
