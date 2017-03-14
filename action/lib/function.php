dfd7a8607b9ddca4cf234e148a583c6b2e81faf6
worldace <tokaiknight@yahoo.co.jp>
2017-03-14 22:16:10 +0900

    no message

--- action/lib/function.php
+++ action/lib/function.php
@@ -1230,26 +1230,25 @@ class 部品{
     }
 
     public static function 作成($部品名, $引数){
-        //部品変数を初期化
-        $html = $css = $cssfile = $js = $jsfile = $jsinhead = "";
-
         $部品パス = self::パス($部品名);
 
         //キャッシュの有無により分岐
         if(!isset(self::$記憶[$部品パス])){
-            require $部品パス;
-            self::$記憶[$部品パス] = $html;
+            $file = file_get_contents($部品パス);
+            preg_match("|<script\s+type\s*=\s*[\"\']*php[\"\']*\s*>([\s\S]*?)</script>|i", $file, $code);
+            eval($code[1] . ";");
+            self::$記憶[$部品パス] = isset($php) ? $php : "";
 
             //部品変数を処理して結果にまとめる
-            self::$結果['css'] .= self::CSS変数処理($css, $cssfile, $引数);
-            if($jsinhead){ self::$結果['jsinhead'] .= self::JS変数処理($js, $jsfile, $引数); }
-            else         { self::$結果['jsinbody'] .= self::JS変数処理($js, $jsfile, $引数); }
+            self::$結果['css'] .= self::CSS処理($file);
+            self::$結果['jsinhead'] .= self::JS処理(substr($file, 0, stripos($file, "</head")));
+            self::$結果['jsinbody'] .= self::JS処理(substr($file, stripos($file, "</head")));
         }
         else{
-            $html = self::$記憶[$部品パス];
+            $php = self::$記憶[$部品パス];
         }
 
-        return is_callable($html)  ?  call_user_func_array($html, $引数)  :  $html;
+        return is_callable($php) ? call_user_func_array($php, $引数) : $php;
     }
 
     public static function 差し込み($buf){
@@ -1286,11 +1285,11 @@ class 部品{
     private static function パス($部品名){
         if(!self::$ディレクトリ){ throw new プログラムミス("部品::開始() を行っていません"); }
 
-        if(preg_match("/\.php$/i", $部品名)){
+        if(preg_match("/\.html$/i", $部品名)){
             $path = (preg_match("#^(/|\\\\|\w+:)#", $部品名))  ?  $部品名  :  dirname(debug_backtrace()[2]['file']) . $部品名; //絶対パスor相対パス
         }
         else{
-            $path = self::$ディレクトリ . "/$部品名.php";
+            $path = self::$ディレクトリ . "/$部品名.html";
         }
         $path = realpath($path);
         if(!$path){ throw new プログラムミス("部品ファイルが見つかりません\n部品名: $部品名\n部品パス: $path"); }
@@ -1298,38 +1297,38 @@ class 部品{
         return $path;
     }
 
-    private static function CSS変数処理($css, $cssfile, $引数){
-        if($css){
-            $css  = is_callable($css)  ?  call_user_func_array($css, $引数)  :  $css;
-            $css  = ltrim($css);
-            $_css = preg_match("/^</", $css)  ?  "$css\n"  :  "<style>\n$css\n</style>\n";
-        }
-        if($cssfile){
-            $cssfile = is_callable($cssfile)  ?  call_user_func_array($cssfile, $引数)  :  $cssfile;
-            foreach((array)$cssfile as $url){
-                if(in_array($url, (array)self::$記憶['読み込み済みURL'])){ continue; }
-                self::$記憶['読み込み済みURL'][] = $url;
-                $_cssfile .= "<link rel=\"stylesheet\" href=\"$url\">\n";
+    private static function CSS処理($html){
+        $return = "";
+        preg_match_all("|<link[^>]+>|i", $html, $link);
+        foreach((array)$link[0] as $v){
+            preg_match("|^([^>]+)|", $v, $attr);
+            if(preg_match("|\s+href\s*=\s*[\"\']([\s\S]*?)[\"\']|i", $attr[0], $href)){
+                if(in_array($href[1], (array)self::$記憶['読み込み済みURL'])){ continue; }
+                self::$記憶['読み込み済みURL'][] = $href[1];
             }
+            $return .= $v . "\n";
         }
-        return $_cssfile . $_css;
+
+        preg_match_all("|<style([\s\S]*?)</style>|i", $html, $style);
+        foreach((array)$style[0] as $v){
+            $return .= $v . "\n";
+        }
+        return $return;
     }
 
-    private static function JS変数処理($js, $jsfile, $引数){
-        if($js){
-            $js  = is_callable($js)  ?  call_user_func_array($js, $引数)  :  $js;
-            $js  = ltrim($js);
-            $_js = preg_match("/^</", $js)  ?  "$js\n"  :  "<script>\n$js\n</script>\n";
-        }
-        if($jsfile){
-            $jsfile = is_callable($jsfile)  ?  call_user_func_array($jsfile, $引数)  :  $jsfile;
-            foreach((array)$jsfile as $url){
-                if(in_array($url, (array)self::$記憶['読み込み済みURL'])){ continue; }
-                self::$記憶['読み込み済みURL'][] = $url;
-                $_jsfile .= "<script src=\"$url\"></script>\n";
+    private static function JS処理($html){
+        $return = "";
+        preg_match_all("|<script([\s\S]*?)</script>|i", $html, $script);
+        foreach((array)$script[0] as $v){
+            preg_match("|^([^>]+)|", $v, $attr);
+            if(preg_match("|\s+type\s*=\s*[\"\']*php|i", $attr[0])){ continue; }
+            if(preg_match("|\s+src\s*=\s*[\"\']([\s\S]*?)[\"\']|i", $attr[0], $src)){
+                if(in_array($src[1], (array)self::$記憶['読み込み済みURL'])){ continue; }
+                self::$記憶['読み込み済みURL'][] = $src[1];
             }
+            $return .= $v . "\n";
         }
-        return $_jsfile . $_js;
+        return $return;
     }
 }
 
