aa422197ea82b38b2b52f0521407a993bf2cb3aa
worldace <tokaiknight@yahoo.co.jp>
2017-03-27 04:18:58 +0900

    no message

--- action/lib/function.php
+++ action/lib/function.php
@@ -1313,7 +1313,7 @@ class 部品{
         self::$記憶['stack'][] = $部品名;
         if(count(self::$記憶['stack']) > 250){ throw new Exception("[$部品名]:ループ数が上限に達しました", 500); }
 
-        $html = is_callable(self::$記憶['部品変数'][$部品名])  ?  call_user_func_array(self::$記憶['部品変数'][$部品名], self::h($引数))  :  self::$記憶['部品変数'][$部品名];
+        $html = is_callable(self::$記憶['部品変数'][$部品名])  ?  self::$記憶['部品変数'][$部品名](...self::h($引数))  :  self::$記憶['部品変数'][$部品名];
 
         array_pop(self::$記憶['stack']);
         return $html;
