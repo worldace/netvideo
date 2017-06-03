10a6433fc12eb729c450bb5900c7c68e85f89264
worldace <tokaiknight@yahoo.co.jp>
2017-06-03 19:14:06 +0900

    no message

--- action/lib/function.php
+++ action/lib/function.php
@@ -124,6 +124,7 @@ function 検査(string $var, $func) :bool{
     }
     if($result === false){
         検査::$失敗 = true;
+        if(検査::$例外 === true){ throw new Exception("検査エラー"); }
     }
     return $result;
 }
@@ -131,6 +132,7 @@ function 検査(string $var, $func) :bool{
 
 class 検査{
     public static $失敗 = false;
+    public static $例外 = false;
 
     public static function 必須($v) :bool{
         return strlen($v) > 0;
