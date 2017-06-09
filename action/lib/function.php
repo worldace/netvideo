adbd836dedfd217eb61f2469fe4392eb2a0cfd2a
worldace <tokaiknight@yahoo.co.jp>
2017-06-09 17:53:51 +0900

    no message

--- action/lib/function.php
+++ action/lib/function.php
@@ -811,7 +811,8 @@ function XML取得(string $xml) :array{
         if($xml === false){ return []; }
     }
     $xml = preg_replace("/&(?!([a-zA-Z0-9]{2,8};)|(#[0-9]{2,5};)|(#x[a-fA-F0-9]{2,4};))/", "&amp;" ,$xml);
-    return json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOBLANKS | LIBXML_NOCDATA | LIBXML_NONET | LIBXML_COMPACT | LIBXML_PARSEHUGE)), true);
+    $SimpleXML = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOBLANKS | LIBXML_NOCDATA | LIBXML_NONET | LIBXML_COMPACT | LIBXML_PARSEHUGE);
+    return json_decode(json_encode([$SimpleXML->getName()=>$SimpleXML]), true);
 }
 
 
