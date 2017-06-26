384add745355bf3e90519d5418a6d84176416c85
worldace <tokaiknight@yahoo.co.jp>
2017-06-26 21:34:38 +0900

    no message

--- action/videopost.php
+++ action/videopost.php
@@ -1,17 +1,17 @@
 <?php
 
-整形("POST", "サムネイル", function($v){ return base64_decode($v); });
+整形($_POST['サムネイル'], function($v){ return base64_decode($v); });
 
-検査("POST", "url", "URL");
-検査("POST", "ユーザid", "必須");
-検査("POST", "ユーザid", "250文字以下");
-検査("POST", "タイトル", "必須");
-検査("POST", "タイトル", "250文字以下");
-検査("POST", "本文", "5000文字以下");
-検査("POST", "横幅", "0より大きい");
-検査("POST", "縦幅", "0より大きい");
-検査("POST", "長さ", "0より大きい");
-検査("POST", "サムネイル", "画像データ");
+検査($_POST['url'], "URL");
+検査($_POST['ユーザid'], "必須");
+検査($_POST['ユーザid'], "250文字以下");
+検査($_POST['タイトル'], "必須");
+検査($_POST['タイトル'], "250文字以下");
+検査($_POST['本文'], "5000文字以下");
+検査($_POST['横幅'], "0より大きい");
+検査($_POST['縦幅'], "0より大きい");
+検査($_POST['長さ'], "0より大きい");
+検査($_POST['サムネイル'], "画像データ");
 
 
 
@@ -42,5 +42,5 @@ file_put_contents("$dir/$id.png", $_POST['サムネイル'], LOCK_EX);
 <meta charset="utf-8">
 <title>投稿成功</title>
 </head>
-<body><?= 部品("jump", URL作成(["action"=>"video","id"=>$id])) ?></body>
+<body><?= 部品("jump", URL(["video",$id])) ?></body>
 </html>
