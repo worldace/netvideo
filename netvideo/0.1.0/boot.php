56345cc93cb14a34a2f585ab385ffcfed6a86fec
worldace <tokaiknight@yahoo.co.jp>
2017-06-27 10:48:10 +0900

    no message

--- netvideo/0.1.0/boot.php
+++ netvideo/0.1.0/boot.php
@@ -5,7 +5,13 @@
 
 ini_set('session.cookie_httponly', 1);
 
+
 $_GET += ['action'=>'', 'id'=>''];
+if(!$_GET['id'] and preg_match("/^\d+$/", $_GET['action'])){
+    $_GET['id']     = $_GET['action'];
+    $_GET['action'] = 'video';
+}
+
 
 自動読み込み(設定['アプリディレクトリ'].'./class/');
 部品::開始(設定['アプリディレクトリ'].'./../部品/');
