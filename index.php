75aa6973990e3c0df1b7a5fd871e906ad0f532be
worldace <tokaiknight@yahoo.co.jp>
2017-07-03 04:59:24 +0900

    no message

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ include './netvideo/0.1.0/function.php';
 include './netvideo/0.1.0/boot.php';
 
 switch($_GET['action']){
-    case ''              : route(['./netvideo/0.1.0/action/index.php']);
-    case 'video'         : route(['./netvideo/0.1.0/action/video.php']);
-    case 'commentget'    : route(['./netvideo/0.1.0/action/commentget.php']);
-    case 'commentpost'   : route(['./netvideo/0.1.0/action/commentpost.php']);
-    case 'videopost'     : route(['./netvideo/0.1.0/action/videopost.php']);
-    case 'videopostform' : route(['./netvideo/0.1.0/action/videopostform.php']);
-    case 'proxy'         : route(['./netvideo/0.1.0/action/proxy.php']);
-    default              : route(['./netvideo/0.1.0/action/index.php']);
+    case ''                : route(['./netvideo/0.1.0/action/index.php']);
+    case 'video'           : route(['./netvideo/0.1.0/action/video.php']);
+    case 'comment-get'     : route(['./netvideo/0.1.0/action/comment-get.php']);
+    case 'comment-post'    : route(['./netvideo/0.1.0/action/comment-post.php']);
+    case 'video-post'      : route(['./netvideo/0.1.0/action/video-post.php']);
+    case 'video-post-form' : route(['./netvideo/0.1.0/action/video-post-form.php']);
+    case 'proxy'           : route(['./netvideo/0.1.0/action/proxy.php']);
+    default                : route(['./netvideo/0.1.0/action/index.php']);
 }
