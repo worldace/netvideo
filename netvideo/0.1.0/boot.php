18781e932659ffeec7b90e312164f174d4b0c8b5
worldace <tokaiknight@yahoo.co.jp>
2017-07-03 13:41:26 +0900

    no message

--- netvideo/0.1.0/boot.php
+++ netvideo/0.1.0/boot.php
@@ -6,9 +6,11 @@ new class{
     function __construct(){
         ini_set('session.cookie_httponly', 1);
 
-        PATH_INFOをGETに代入();
-        $_GET['action'] = $_GET['PATH_INFO'][0] ?? '';
-        $_GET['id']     = $_GET['PATH_INFO'][1] ?? '';
+        //PATH_INFOの仕様＝action.id ※idは存在しない時がある。actionが数字の時は特別
+        $_SERVER['PATH_INFO'] = $_SERVER['PATH_INFO'] ?? '/';
+        preg_match("|^/?([^\.]+)\.?(.*)$|", $_SERVER['PATH_INFO'], $match);
+        $_GET['action'] = $match[1];
+        $_GET['id']     = $match[2];
         if(preg_match("/^\d+$/", $_GET['action'])){
             $_GET['id']     = $_GET['action'];
             $_GET['action'] = 'video';
