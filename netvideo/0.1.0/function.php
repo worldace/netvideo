0d66085225740cd325793575936b9e502a79c9cf
worldace <tokaiknight@yahoo.co.jp>
2017-07-07 01:26:19 +0900

    no message

--- netvideo/0.1.0/function.php
+++ netvideo/0.1.0/function.php
@@ -1169,7 +1169,7 @@ function CSV読み込み(string $path, string $区切り = ",", string $from = "
     }
 
     if(preg_match("/^auto$/i", $from)){
-        $from = mb_detect_encoding(fread($fp, 2048), ["utf-8", "sjis-win", "eucjp-win", "ascii", "ISO-2022-JP"]);
+        $from = mb_detect_encoding(fgets($fp).fgets($fp).fgets($fp), ["utf-8", "sjis-win", "eucjp-win", "ascii", "iso-2022-jp"]);
         if(!$from){
             $from = "utf-8";
         }
